e57675ec1490f92b8a6849beabb5c4578a793ef3
Marco Roth <marco.roth@intergga.ch>
2025-02-11 21:17:03 +0100

    Add test execution timeouts by overriding `Minitest::Spec#run`

--- ext/erbx/test/test_helper.rb
+++ ext/erbx/test/test_helper.rb
@@ -1,11 +1,48 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "erbx"
 require "maxitest/autorun"
+require "timeout"
 
 class Minitest::Spec
+  TIMEOUT_THRESHOLD = 0.5 # seconds
+
   class << self
     alias_method :test, :it
     alias_method :xtest, :xit
+  end
+
+  def run
+    reader, writer = IO.pipe
+
+    pid = fork do
+      reader.close
+      result = super
+      writer.write(Marshal.dump(result)) # Serialize result back to parent
+      writer.close
+      exit! # Avoid running at_exit hooks
+    end
+
+    writer.close
+
+    begin
+      Timeout.timeout(TIMEOUT_THRESHOLD) do
+        Process.wait(pid) # Wait for the test to finish
+      end
+
+      result = Marshal.load(reader.read) # Retrieve test result
+      result
+
+    rescue Timeout::Error, Timeout::ExitException
+      Process.kill("TERM", pid) # Gracefully terminate
+
+      sleep 0.5 # Give it time to exit
+
+      Process.kill("KILL", pid) rescue nil # Force kill if needed
+
+      self.fail "Test exceeded timeout of #{TIMEOUT_THRESHOLD} seconds"
+    ensure
+      reader.close
+    end
   end
 end
