6f006fedddacb2d2bee07e2740af24d658bd1998
Marco Roth <marco.roth@intergga.ch>
2025-02-14 20:10:21 +0100

    Restore old behavior

--- src/buffer.c
+++ src/buffer.c
@@ -56,11 +56,20 @@ bool buffer_increase_capacity(buffer_T* buffer, size_t required_length) {
 }
 
 void buffer_append(buffer_T* buffer, const char* text) {
-  if (!text || text[0] == '\0') return;
-
   size_t text_length = strlen(text);
 
-  if (!buffer_increase_capacity(buffer, text_length)) return;
+  if (buffer->length + text_length >= buffer->capacity) {
+    size_t new_capacity = (buffer->length + text_length) * 2;
+    char* new_buffer = realloc(buffer->value, new_capacity);
+
+    if (new_buffer) {
+      buffer->value = new_buffer;
+      buffer->capacity = new_capacity;
+    } else {
+      printf("Couldn't allocate memory for new_buffer in buffer_append");
+      return;
+    }
+  }
 
   strcat(buffer->value + buffer->length, text);
   buffer->length += text_length;
@@ -76,20 +85,16 @@ void buffer_append_char(buffer_T* buffer, char character) {
 
 void buffer_append_repeated(buffer_T* buffer, char character, size_t length) {
   if (length <= 0) return;
-
   if (buffer->length + length >= buffer->capacity) {
     size_t new_capacity = (buffer->length + length) * 2;
     char* new_buffer = realloc(buffer->value, new_capacity);
-
     if (!new_buffer) {
       printf("Couldn't allocate memory for new_buffer in buffer_append_repeated\n");
       return;
     }
-
     buffer->value = new_buffer;
     buffer->capacity = new_capacity;
   }
-
   memset(buffer->value + buffer->length, character, length);
   buffer->length += length;
   buffer->value[buffer->length] = '\0';
@@ -100,25 +105,38 @@ void buffer_append_whitespace(buffer_T* buffer, size_t length) {
 }
 
 void buffer_prepend(buffer_T* buffer, const char* text) {
-  if (!text || text[0] == '\0') return;
+  if (text == NULL || text[0] == '\0') return;
 
   size_t text_length = strlen(text);
+  size_t new_length = buffer->length + text_length;
 
-  if (!buffer_increase_capacity(buffer, text_length)) return;
+  if (new_length >= buffer->capacity) {
+    size_t new_capacity = new_length * 2;
+    buffer->value = realloc(buffer->value, new_capacity);
+    buffer->capacity = new_capacity;
+  }
 
   memmove(buffer->value + text_length, buffer->value, buffer->length + 1);
   memcpy(buffer->value, text, text_length);
 
-  buffer->length += text_length;
+  buffer->length = new_length;
+  buffer->value[buffer->length] = '\0';
 }
 
 void buffer_concat(buffer_T* destination, buffer_T* source) {
   if (source->length == 0) return;
-  if (!buffer_increase_capacity(destination, source->length)) return;
 
-  memcpy(destination->value + destination->length, source->value, source->length);
-  destination->length += source->length;
-  destination->value[destination->length] = '\0';
+  size_t new_length = destination->length + source->length;
+
+  if (new_length >= destination->capacity) {
+    size_t new_capacity = new_length * 2;
+    destination->value = realloc(destination->value, new_capacity);
+    destination->capacity = new_capacity;
+  }
+
+  strcat(destination->value + destination->length, source->value);
+
+  destination->length = new_length;
 }
 
 bool buffer_reserve(buffer_T* buffer, size_t min_capacity) {
@@ -133,10 +151,7 @@ void buffer_clear(buffer_T* buffer) {
 }
 
 void buffer_free(buffer_T* buffer) {
-  if (!buffer) return;
-
   free(buffer->value);
-
   buffer->value = NULL;
   buffer->length = buffer->capacity = 0;
 }
