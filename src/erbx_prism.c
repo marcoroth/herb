01a5d43c913ea51cf1564b129a3e3305ece8fcdc
Marco Roth <marco.roth@intergga.ch>
2025-02-15 01:31:51 +0100

    Extract Ruby from HTML+ERB file and pass it to Prism

--- src/erbx_prism.c
+++ src/erbx_prism.c
@@ -1,5 +1,6 @@
 #include "include/io.h"
 #include "include/ruby_parser.h"
+#include "include/extract.h"
 
 #include <stdio.h>
 
@@ -10,9 +11,13 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  char* source = erbx_read_file(argv[1]);
+  char* html_erb_source = erbx_read_file(argv[1]);
+  printf("HTML+ERB File: \n%s\n", html_erb_source);
 
-  erbx_parse_ruby(source);
+  char* ruby_source = erbx_extract(html_erb_source, ERBX_EXTRACT_LANGUAGE_RUBY);
+  printf("Extracted Ruby: \n%s\n", ruby_source);
+
+  erbx_parse_ruby(ruby_source);
 
   return 0;
 }
