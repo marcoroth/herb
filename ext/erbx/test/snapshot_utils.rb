e80e1ccf469bd6dd9f08fb12e6fd58eb718d8b5f
Marco Roth <marco.roth@intergga.ch>
2025-02-17 02:58:58 +0100

    Update snapshot utils

--- ext/erbx/test/snapshot_utils.rb
+++ ext/erbx/test/snapshot_utils.rb
@@ -1,19 +1,81 @@
 # frozen_string_literal: true
 
 require "fileutils"
+require "readline"
+
+def ask?(prompt = "")
+  Readline.readline("===> #{prompt}? (y/N) ", true).squeeze(" ").strip == "y"
+end
 
 module SnapshotUtils
-  def save_failures_to_snapshot(content)
-    puts "Updating Snapshot for #{name} at: #{snapshot_file}"
+  def snapshot_changed?(content)
+    if snapshot_file.exist?
+      previous_content = snapshot_file.read
 
-    FileUtils.mkdir_p(snapshot_file.dirname)
-    snapshot_file.write(content)
+      if previous_content == content
+        puts "Snapshot for #{name} didn't change: #{snapshot_file}"
+        false
+      else
+        puts "Snapshot for '#{class_name} #{name}' changed:\n"
+
+        puts Difftastic::Differ.new(color: :always).diff_strings(previous_content, content)
+        puts "==============="
+        true
+      end
+    else
+      puts "Snapshot for #{name} doesn't exist at: #{snapshot_file}"
+      true
+    end
   end
 
-  def assert_snapshot_matches(actual)
+  def save_failures_to_snapshot(content, source)
+    return unless snapshot_changed?(content)
+
+    puts "\n==== [ Input for '#{class_name} #{name}' ] ====="
+    puts source
+    puts "\n\n"
+
+    if ask?("Do you want to update the snapshot for '#{class_name} #{name}'?")
+
+      puts "Updating Snapshot for #{name} at: #{snapshot_file}..."
+
+      FileUtils.mkdir_p(snapshot_file.dirname)
+      snapshot_file.write(content)
+
+      puts "Snapshot for #{name} written: #{snapshot_file}"
+    else
+      puts "Not updating snapshot for #{name} at #{snapshot_file}."
+    end
+  end
+
+  def assert_snapshot_matches(actual, source)
     assert snapshot_file.exist?, "Expected snapshot file to exist: #{snapshot_file.to_path}"
 
     assert_equal snapshot_file.read, actual
+  rescue Minitest::Assertion => e
+    save_failures_to_snapshot(actual, source) if ENV["UPDATE_SNAPSHOTS"]
+
+    if snapshot_file.read != actual
+      puts
+
+      divider = "=" * `tput cols`.strip.to_i
+
+      flunk(<<~MESSAGE)
+        \e[0m
+        #{divider}
+        #{Difftastic::Differ.new(color: :always).diff_strings(snapshot_file.read, actual)}
+        \e[31m#{divider}
+
+        Snapshots for "#{class_name} #{name}" didn't match.
+
+        Run the test using UPDATE_SNAPSHOTS=true to update the snapshot file for "#{name}"
+
+        UPDATE_SNAPSHOTS=true mtest #{e.location}
+
+        #{divider}
+        \e[0m
+      MESSAGE
+    end
   end
 
   def snapshot_file
