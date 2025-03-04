b204ef70721c4255c8ab75c7758c0d92c88c573f
Marco Roth <marco.roth@intergga.ch>
2025-03-04 19:39:18 +0100

    Move helpers to `extension_helpers.c`

--- ext/erbx/extension.c
+++ ext/erbx/extension.c
@@ -4,55 +4,8 @@
 #include "extension_helpers.h"
 #include "nodes.h"
 
-#include "../../src/include/ast_pretty_print.h"
 #include "../../src/include/erbx.h"
-#include "../../src/include/io.h"
 #include "../../src/include/token.h"
-
-VALUE LexResult;
-VALUE ParseResult;
-
-static VALUE create_lex_result(array_T* tokens, VALUE source) {
-  VALUE value = rb_ary_new();
-  VALUE warnings = rb_ary_new();
-  VALUE errors = rb_ary_new();
-
-  for (size_t i = 0; i < array_size(tokens); i++) {
-    token_T* token = array_get(tokens, i);
-    if (token != NULL) { rb_ary_push(value, rb_token_from_c_struct(token)); }
-  }
-
-  erbx_free_tokens(&tokens);
-  VALUE args[4] = { value, source, warnings, errors };
-  return rb_class_new_instance(4, args, LexResult);
-}
-
-static VALUE create_parse_result(AST_DOCUMENT_NODE_T* root, VALUE source) {
-  buffer_T output;
-  if (!buffer_init(&output)) { return Qnil; }
-
-  VALUE value = rb_node_from_c_struct((AST_NODE_T*) root);
-  VALUE warnings = rb_ary_new();
-  VALUE errors = rb_ary_new();
-
-  if (root) {
-    ast_pretty_print_node((AST_NODE_T*) root, 0, 0, &output);
-    ast_node_free((AST_NODE_T*) root);
-  }
-
-  buffer_free(&output);
-  VALUE args[4] = { value, source, warnings, errors };
-  return rb_class_new_instance(4, args, ParseResult);
-}
-
-static VALUE read_file_to_ruby_string(const char* file_path) {
-  char* source = erbx_read_file(file_path);
-  VALUE source_value = rb_str_new_cstr(source);
-
-  free(source);
-
-  return source_value;
-}
 
 static VALUE ERBX_lex(VALUE self, VALUE source) {
   char* string = (char*) check_string(source);
@@ -143,10 +96,6 @@ static VALUE ERBX_version(VALUE self) {
 
 void Init_erbx(void) {
   VALUE ERBX = rb_define_module("ERBX");
-  VALUE Result = rb_define_class_under(ERBX, "Result", rb_cObject);
-
-  LexResult = rb_define_class_under(ERBX, "LexResult", Result);
-  ParseResult = rb_define_class_under(ERBX, "ParseResult", Result);
 
   rb_define_singleton_method(ERBX, "parse", ERBX_parse, 1);
   rb_define_singleton_method(ERBX, "lex", ERBX_lex, 1);
