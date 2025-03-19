b4ee859a35957aaf5513d396f4152f4253f34a86
Marco Roth <marco.roth@intergga.ch>
2025-03-19 03:49:00 +0100

    Upgrade Prism to v1.4.0

--- src/herb.c
+++ src/herb.c
@@ -9,6 +9,7 @@
 #include "include/version.h"
 
 #include <stdlib.h>
+#include <prism.h>
 
 array_T* herb_lex(const char* source) {
   lexer_T* lexer = lexer_init(source);
@@ -95,4 +96,8 @@ void herb_free_tokens(array_T** tokens) {
 
 const char* herb_version(void) {
   return HERB_VERSION;
+}
+
+const char* herb_prism_version(void) {
+  return PRISM_VERSION;
 }
