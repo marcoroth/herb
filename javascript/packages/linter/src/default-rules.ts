8f05ab580de56d9feaaa261b156432b8de2f1c91
fresh <drewsdhoffer@gmail.com>
2025-07-01 15:16:15 -0400

    add whitespcae rule to default rules

--- javascript/packages/linter/src/default-rules.ts
+++ javascript/packages/linter/src/default-rules.ts
@@ -1,6 +1,7 @@
 import type { RuleClass } from "./types.js"
 
 import { ERBNoOutputControlFlow } from "./rules/erb-no-output-control-flow.js"
+import { ERBRequireWhitespaceRule } from "./rules/erb-require-whitespace-inside-tags.js"
 import { HTMLTagNameLowercaseRule } from "./rules/html-tag-name-lowercase.js"
 import { HTMLNoDuplicateAttributesRule } from "./rules/html-no-duplicate-attributes.js"
 import { HTMLImgRequireAltRule } from "./rules/html-img-require-alt.js"
@@ -11,8 +12,10 @@ import { HTMLBooleanAttributesNoValueRule } from "./rules/html-boolean-attribute
 import { HTMLNoBlockInsideInlineRule } from "./rules/html-no-block-inside-inline.js"
 import { HTMLNoEmptyHeadingsRule } from "./rules/html-no-empty-headings.js"
 
+
 export const defaultRules: RuleClass[] = [
   ERBNoOutputControlFlow,
+  ERBRequireWhitespaceRule,
   HTMLTagNameLowercaseRule,
   HTMLNoDuplicateAttributesRule,
   HTMLImgRequireAltRule,
