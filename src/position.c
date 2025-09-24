c855109d0abdffd4e70afc2adb759b1954a2d4ab
Tim Kächele <mail@timkaechele.me>
2025-09-24 21:56:07 +0200

    Clean up position implementation
    - Remove obsolete functions
    - Use 32bit unsigned ints for line/column
    - Use pass by value instead of pointers

--- src/position.c
+++ src/position.c
@@ -5,7 +5,7 @@ size_t position_sizeof(void) {
   return sizeof(position_T);
 }
 
-position_T* position_init(const size_t line, const size_t column) {
+position_T* position_init(const uint32_t line, const uint32_t column) {
   position_T* position = safe_malloc(position_sizeof());
 
   position->line = line;
@@ -14,20 +14,8 @@ position_T* position_init(const size_t line, const size_t column) {
   return position;
 }
 
-size_t position_line(const position_T* position) {
-  return position->line;
-}
-
-size_t position_column(const position_T* position) {
-  return position->column;
-}
-
 position_T* position_copy(position_T* position) {
   if (position == NULL) { return NULL; }
 
-  return position_init(position_line(position), position_column(position));
-}
-
-void position_free(position_T* position) {
-  free(position);
+  return position_init(position->line, position->column);
 }
