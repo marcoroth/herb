44ef0674bd183e2592bc052a274318b7760de32c
Marco Roth <marco.roth@intergga.ch>
2025-10-01 16:43:27 +0900

    Parser: Fix parsing boolean attributes with `track_whitespace: true`

--- test/parser/boolean_attributes_test.rb
+++ test/parser/boolean_attributes_test.rb
@@ -29,5 +29,13 @@ module Parser
     test "boolean attribute surrounded by regular attributes" do
       assert_parsed_snapshot(%(<input class="classes" required id="ids"/>))
     end
+
+    test "boolean attribute on void element followed by newline and ERB tag with track_whitespace" do
+      assert_parsed_snapshot(%(<link crossorigin>\n<%= hello %>), track_whitespace: true)
+    end
+
+    test "boolean attribute on void element followed by ERB tag with track_whitespace" do
+      assert_parsed_snapshot(%(<link crossorigin><%= hello %>), track_whitespace: true)
+    end
   end
 end
