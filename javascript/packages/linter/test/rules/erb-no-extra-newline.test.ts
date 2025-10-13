86a0a2463cbefb1a21332de2a395ce53b116b90b
Marco Roth <marco.roth@intergga.ch>
2025-10-13 10:41:43 +0200

    Use new linter test helpers

--- javascript/packages/linter/test/rules/erb-no-extra-newline.test.ts
+++ javascript/packages/linter/test/rules/erb-no-extra-newline.test.ts
@@ -1,81 +1,55 @@
 import dedent from "dedent"
 
-import { describe, test, expect, beforeAll } from "vitest"
-import { Herb } from "@herb-tools/node-wasm"
-import { Linter } from "../../src/linter.js"
+import { describe, test } from "vitest"
+import { createLinterTest } from "../helpers/linter-test-helper.js"
 
 import { ERBNoExtraNewLineRule } from "../../src/rules/erb-no-extra-newline.js"
 
-describe("ERBNoExtraNewLineRule", () => {
-  beforeAll(async () => {
-    await Herb.load()
-  })
+const { expectNoOffenses, expectError, assertOffenses } = createLinterTest(ERBNoExtraNewLineRule)
 
+describe("erb-no-extra-newline", () => {
   test("when no new line is present", () => {
-    const html = dedent`
+    expectNoOffenses(dedent`
       line 1
-    `
-    const linter = new Linter(Herb, [ERBNoExtraNewLineRule])
-    const lintResult = linter.lint(html)
-
-    expect(lintResult.errors).toBe(0)
-    expect(lintResult.warnings).toBe(0)
-    expect(lintResult.offenses).toHaveLength(0)
+    `)
   })
 
   test("when no blank lines are present", () => {
-    const html = dedent`
+    expectNoOffenses(dedent`
       line 1
       line 2
       line 3
-    `
-    const linter = new Linter(Herb, [ERBNoExtraNewLineRule])
-    const lintResult = linter.lint(html)
-
-    expect(lintResult.offenses).toHaveLength(0)
+    `)
   })
 
   test("when a single blank line is present", () => {
-    const html = dedent`
+    expectNoOffenses(dedent`
       line 1
 
       line 3
-    `
-    const linter = new Linter(Herb, [ERBNoExtraNewLineRule])
-    const lintResult = linter.lint(html)
-
-    expect(lintResult.offenses).toHaveLength(0)
+    `)
   })
 
   test("when two blank lines follow each other", () => {
-    const html = dedent`
+    expectError("Extra blank line detected.")
+
+    assertOffenses(dedent`
       line 1
 
 
       line 3
-    `
-    const linter = new Linter(Herb, [ERBNoExtraNewLineRule])
-    const lintResult = linter.lint(html)
-
-    expect(lintResult.offenses).toHaveLength(1)
-    expect(lintResult.offenses[0].message).toBe("Extra blank line detected.")
+    `)
   })
 
   test("when more than two newlines follow each other", () => {
-    let html = dedent`
+    expectError("Extra blank line detected.")
+
+    assertOffenses(dedent`
       line 1
 
 
 
       line 3
-    `
-
-    html += "line1".repeat(300_000)
-
-    const linter = new Linter(Herb, [ERBNoExtraNewLineRule])
-    const lintResult = linter.lint(html)
-
-    expect(lintResult.offenses).toHaveLength(1)
-    expect(lintResult.offenses[0].message).toBe("Extra blank line detected.")
+    `)
   })
 })
