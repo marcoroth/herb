2c519a5d74d30cb5bdd0c3dc4d2d4af648c1e9af
Tim Kächele <mail@timkaechele.me>
2025-10-18 18:54:23 +0200

    Change quoted_string function to take hb_string_T

--- src/include/util.h
+++ src/include/util.h
@@ -1,13 +1,14 @@
 #ifndef HERB_UTIL_H
 #define HERB_UTIL_H
 
+#include "util/hb_string.h"
 #include <stdbool.h>
 #include <stdlib.h>
 
 int is_newline(int character);
 
 char* escape_newlines(const char* input);
-char* quoted_string(const char* input);
+hb_string_T quoted_string(hb_string_T input);
 char* herb_strdup(const char* s);
 
 #endif
