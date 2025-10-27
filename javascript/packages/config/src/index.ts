bc2453f515101869d80f6194c26720b6628e8981
Marco Roth <marco.roth@intergga.ch>
2025-10-28 00:53:01 +0100

    Introduce `@herb-tools/config` and `.herb.yml` configuration file

---
+++ javascript/packages/config/src/index.ts
@@ -0,0 +1,14 @@
+export { Config } from "./config.js"
+export { HerbConfigSchema } from "./config-schema.js"
+
+export type {
+  HerbConfig,
+  HerbConfigOptions,
+  LinterConfig,
+  FormatterConfig,
+  RuleConfig,
+  FilesConfig,
+  LoadOptions,
+  FromObjectOptions,
+  ConfigValidationError
+} from "./config.js"
