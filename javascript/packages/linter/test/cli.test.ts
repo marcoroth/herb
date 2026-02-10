124d1cf0fe9ee420b0ee31a5a98a039cd67f126b
Marco Roth <marco.roth@intergga.ch>
2026-02-11 00:08:19 +0100

    `v0.8.10`
    https://github.com/marcoroth/herb/releases/tag/v0.8.10

--- javascript/packages/linter/test/cli.test.ts
+++ javascript/packages/linter/test/cli.test.ts
@@ -566,7 +566,7 @@ describe("CLI Output Formatting", () => {
         mkdirSync(join(tempDir, "app/views"), { recursive: true })
 
         writeFileSync(join(tempDir, ".herb.yml"), dedent`
-          version: 0.8.9
+          version: 0.8.10
           linter:
             enabled: true
         `)
@@ -592,7 +592,7 @@ describe("CLI Output Formatting", () => {
         mkdirSync(join(tempDir, "app/views"), { recursive: true })
 
         writeFileSync(join(tempDir, ".herb.yml"), dedent`
-          version: 0.8.9
+          version: 0.8.10
           linter:
             enabled: true
         `)
@@ -642,7 +642,7 @@ describe("CLI Output Formatting", () => {
         mkdirSync(join(tempDir, "app/views/widgets"), { recursive: true })
 
         writeFileSync(join(tempDir, ".herb.yml"), dedent`
-          version: 0.8.9
+          version: 0.8.10
           linter:
             enabled: true
         `)
