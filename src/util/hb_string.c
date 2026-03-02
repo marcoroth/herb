0e0b60ac41514278b40798302d9a7e318274d8d8
Marco Roth <marco.roth@intergga.ch>
2026-03-02 04:01:16 +0100

    Format

--- src/util/hb_string.c
+++ src/util/hb_string.c
@@ -63,13 +63,11 @@ hb_string_T hb_string_range(hb_string_T string, uint32_t from, uint32_t to) {
 }
 
 hb_string_T hb_string_copy(hb_string_T string) {
-  if (hb_string_is_empty(string)) {
-    return (hb_string_T){ .data = NULL, .length = 0 };
-  }
+  if (hb_string_is_empty(string)) { return (hb_string_T) { .data = NULL, .length = 0 }; }
 
   char* copy = hb_string_to_c_string_using_malloc(string);
 
-  return (hb_string_T){ .data = copy, .length = string.length };
+  return (hb_string_T) { .data = copy, .length = string.length };
 }
 
 char* hb_string_to_c_string_using_malloc(hb_string_T string) {
