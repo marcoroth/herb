fac8b7b7e729be4b9a7213e602213242979ea794
Marco Roth <marco.roth@intergga.ch>
2026-04-13 11:50:55 -0300

    `v0.9.6`

--- javascript/packages/node-wasm/test/node-wasm.test.ts
+++ javascript/packages/node-wasm/test/node-wasm.test.ts
@@ -20,7 +20,7 @@ describe("@herb-tools/node-wasm", () => {
   test("version() returns a string", async () => {
     const version = Herb.version
     expect(typeof version).toBe("string")
-    expect(version).toBe("@herb-tools/node-wasm@0.9.5, @herb-tools/core@0.9.5, libprism@1.9.0, libherb@0.9.5 (WebAssembly)")
+    expect(version).toBe("@herb-tools/node-wasm@0.9.6, @herb-tools/core@0.9.6, libprism@1.9.0, libherb@0.9.6 (WebAssembly)")
   })
 
   test("parse() can process a simple template", async () => {
