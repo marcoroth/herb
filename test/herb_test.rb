3fe5869484239f37a2e7547e134c14479cde1202
Marco Roth <marco.roth@intergga.ch>
2026-04-13 11:30:30 -0300

    `v0.9.6`

--- test/herb_test.rb
+++ test/herb_test.rb
@@ -4,6 +4,6 @@ require_relative "test_helper"
 
 class HerbTest < Minitest::Spec
   test "version" do
-    assert_equal "herb gem v0.9.5, libprism v1.9.0, libherb v0.9.5 (Ruby C native extension)", Herb.version
+    assert_equal "herb gem v0.9.6, libprism v1.9.0, libherb v0.9.6 (Ruby C native extension)", Herb.version
   end
 end
