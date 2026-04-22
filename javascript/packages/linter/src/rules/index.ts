8791cd4fab6d232447221a86d05f040250f4aa64
Joel Hawksley <joelhawksley@github.com>
2026-04-22 08:53:55 -0600

    Implement `a11y-svg-has-accessible-text` linter rule
    Add a new linter rule that ensures `<svg>` elements have accessible text.
    The rule reports an offense when an `<svg>` is missing all of the following:
    
    - `aria-label` attribute
    - `aria-labelledby` attribute
    - A nested `<title>` child element
    
    SVGs marked as decorative with `aria-hidden="true"` are exempt.
    
    Based on the erblint-github `GitHub::Accessibility::SvgHasAccessibleText` rule.
    
    Closes #1226

--- javascript/packages/linter/src/rules/index.ts
+++ javascript/packages/linter/src/rules/index.ts
@@ -1,6 +1,7 @@
 export * from "./a11y-no-accesskey-attribute.js"
 export * from "./a11y-no-aria-unsupported-elements.js"
 export * from "./a11y-no-autofocus-attribute.js"
+export * from "./a11y-svg-has-accessible-text.js"
 
 export * from "./rule-utils.js"
 export * from "./prism-rule-utils.js"
