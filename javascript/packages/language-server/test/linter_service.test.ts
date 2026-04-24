f681767b9b97ef91605cb6fc59deccfdba18f50a
Marco Roth <marco.roth@intergga.ch>
2026-04-24 16:27:00 +0900

    `v0.10.1`
    https://github.com/marcoroth/herb/releases/tag/v0.10.1

--- javascript/packages/language-server/test/linter_service.test.ts
+++ javascript/packages/language-server/test/linter_service.test.ts
@@ -231,7 +231,7 @@ describe("LinterService", () => {
       settings.projectConfig = {
         path: "/test/.herb.yml",
         config: {
-          version: "0.10.0",
+          version: "0.10.1",
           linter: {
             enabled: true,
             rules: {
