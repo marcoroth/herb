76bee7e6f6998d8364f3954342cc02ae39c2517f
Marco Roth <marco.roth@intergga.ch>
2026-08-13 21:17:11 +0200

    Fomat

--- src/herb.c
+++ src/herb.c
@@ -40,7 +40,11 @@ static bool herb_count_node_errors(const AST_NODE_T* node, void* data) {
   return true;
 }
 
-HERB_EXPORTED_FUNCTION AST_DOCUMENT_NODE_T* herb_parse(const char* source, const parser_options_T* options, hb_allocator_T* allocator) {
+HERB_EXPORTED_FUNCTION AST_DOCUMENT_NODE_T* herb_parse(
+  const char* source,
+  const parser_options_T* options,
+  hb_allocator_T* allocator
+) {
   if (!source) { source = ""; }
 
   lexer_T lexer = { 0 };
