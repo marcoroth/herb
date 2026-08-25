3ec2056e90e6f139ef57eec0cc5ce9ff09757e30
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2026-08-25 10:19:10 -0300

    restore javascript/packages/vscode/.vscode-test.mjs

--- javascript/packages/vscode/.vscode-test.mjs
+++ javascript/packages/vscode/.vscode-test.mjs
@@ -2,8 +2,4 @@ import { defineConfig } from '@vscode/test-cli'
 
 export default defineConfig({
   files: 'out/test/**/*.test.js',
-  workspaceFolder: 'test-fixture',
-  mocha: {
-    timeout: 15_000,
-  },
 })
