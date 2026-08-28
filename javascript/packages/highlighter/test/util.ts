44df89dfa0ad31fa39ff4cd3bac7abe9b03519e4
Marco Roth <marco.roth@intergga.ch>
2026-08-28 04:15:39 +0200

    Highlighter: Ship stripAnsiColors next to the regex it wraps

--- javascript/packages/highlighter/test/util.ts
+++ javascript/packages/highlighter/test/util.ts
@@ -1,5 +1,1 @@
-import { ANSI_REGEX } from "../src/ansi.js"
-
-export const stripAnsiColors = (text: string): string => {
-  return text.replace(ANSI_REGEX, "")
-}
+export { stripAnsiColors } from "../src/ansi.js"
