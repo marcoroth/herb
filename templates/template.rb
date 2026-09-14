0995e40412e942a48051372fc7bd39a03ccc9bcc
Marco Roth <marco.roth@intergga.ch>
2026-09-14 15:32:16 +0200

    Ruby: Sort and improve `Herb::ParserOptions` inspect output

--- templates/template.rb
+++ templates/template.rb
@@ -1258,10 +1258,6 @@ module Herb
         "()I"
       end
 
-      def ruby_inspect_value
-        string_array? ? "@#{snake_case}.inspect" : "@#{snake_case}"
-      end
-
       def ruby_constant?
         !string_array?
       end
